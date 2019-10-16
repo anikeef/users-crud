04eb2c6147b477915218cd7fceb441b3b9383c1d
Gennadiy <genaydzhan70@gmail.com>
2019-10-16 20:39:52 +0600

    Updates

---
+++ precache-manifest.df3a424dbdc5888c9617ada5d1f7d4ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68708a3bcc2a073dffa4f5b89550e61e",
+    "url": "/users-crud/index.html"
+  },
+  {
+    "revision": "f56d4c83edd17b64c667",
+    "url": "/users-crud/static/css/main.860b3634.chunk.css"
+  },
+  {
+    "revision": "bb1ae44f223883ba58a7",
+    "url": "/users-crud/static/js/2.3d32d963.chunk.js"
+  },
+  {
+    "revision": "f56d4c83edd17b64c667",
+    "url": "/users-crud/static/js/main.e3ffcbd4.chunk.js"
+  },
+  {
+    "revision": "9f1664af2650a8bb2382",
+    "url": "/users-crud/static/js/runtime-main.c10774da.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/users-crud/precache-manifest.df3a424dbdc5888c9617ada5d1f7d4ca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/users-crud/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
